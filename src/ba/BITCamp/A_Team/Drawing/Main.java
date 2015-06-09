b9fc54cca91b4bd5e4ef035a041c04b1b88fe1c4
Ajla El Tabari <ajla.eltabari@BitCamps-Mac-mini-65.local>
2015-06-09 17:30:28 +0200

    modifications

--- src/ba/BITCamp/A_Team/Drawing/Main.java
+++ src/ba/BITCamp/A_Team/Drawing/Main.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class Main {
 
@@ -16,112 +17,117 @@ public class Main {
 		BufferedImage img = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
 		Graphics g = img.getGraphics();
 		// **** Draw here ****
+		TextIO.readFile("src/ba/BITCamp/A_Team/Drawing/ConfigurationFile");
 		
-		//Task4
-		g.setColor(Color.LIGHT_GRAY);
-    	g.fillRect(0, 0, 800, 800);
-
 		g.setColor(Color.BLACK);
-    	for (int i = 50; i < 100; i+=50) {
-    		for (int j = 50; j < 100; j+=50) {
-	    		if (i % 2 == 0 && j % 2 == 0) {
-	    			g.drawRect(i, j, 50, 50);
-	    			g.fillRect(i, j, 50, 50);
-	    		}
-	    		else if (i % 2 != 0 && j % 2 != 0) {
-	    			g.drawRect(i, j, 50, 50);
-	    			g.fillRect(i, j, 50, 50);
-	    		}
-    		}
-    	}
-    		
-
 		
-		//Task3
-		/*g.setColor(Color.PINK);
-    	g.fillRect(0, 0, 1200, 800);
-    	g.setColor(Color.BLACK);
-    	
-    	g.drawOval(175, 90, 110, 150);
-    	g.drawOval(240, 90, 110, 150);
-    	
-    	g.setColor(Color.RED);
-		g.setFont(new Font("Cambria", Font.PLAIN, 18));
-		g.drawString(".1", 250, 150);
-		g.drawString(".12", 215, 135);
-		g.drawString(".3", 200, 175);
-		g.drawString(".4", 260, 185);
-		g.drawString(".11", 310, 160);
-		g.drawString(".6", 225, 220);
-
-		g.drawString("A", 185, 80);
-		g.drawString("B", 315, 80);
-		
-
-		g.drawString("A = {12, 3, 6}", 100, 300);
-		g.drawString("B = {11}", 100, 350);
-
-		g.drawString("Presjek = {1, 4}", 270, 320);*/
-		
-		// Task2
-		/*g.setColor(Color.PINK);
-    	g.fillRect(0, 0, 1200, 800);
-    	g.setColor(Color.BLACK);
-    	
-    	g.setColor(Color.WHITE);
-    	g.fillOval(60, 90, 90, 150);
-    	g.drawOval(60, 90, 90, 150);
-    	g.setColor(Color.YELLOW);
-    	g.drawOval(220, 90, 90, 150);
-    	g.fillOval(220, 90, 90, 150);
-    	
-    	g.setColor(Color.RED);
-		g.setFont(new Font("Cambria", Font.PLAIN, 18));
-		g.drawString(".1", 250, 150);
-		g.drawString(".12", 235, 135);
-		g.drawString(".3", 235, 175);
-		g.drawString(".4", 270, 185);
-		g.drawString(".11", 275, 145);
-		g.drawString(".6", 250, 220);
-		
-
-		g.drawString(".7", 90, 150);
-		g.drawString(".8", 75, 135);
-		g.drawString(".9", 75, 175);
-		g.drawString(".10", 100, 185);
-		g.drawString(".5", 120, 155);
-		g.drawString(".2", 110, 220);
-
-		g.drawString("A", 70, 80);
-		g.drawLine(70, 85, 85, 95);
-		g.drawString("B", 230, 80);
-		g.drawLine(230, 85, 240, 100);
-		
-
-		g.drawString("A = {1, 12, 3, 4, 11, 6}", 100, 300);
-		g.drawString("B = {7, 8, 9, 10, 5, 2}", 100, 350);*/
-		
-		
-		// Task1
-		/*g.setColor(Color.BLACK);
-    	g.fillRect(0, 0, 1200, 800);
-    	g.setColor(Color.WHITE);
-    	
-    	g.drawOval(130, 70, 70, 150);
-    	g.drawLine(330, 70, 330, 220);
-    	g.drawLine(330, 70, 280, 180);*/
-		
-		/*g.setColor(Color.ORANGE);
-		g.drawOval(100, 100, 400, 100);
-		
-		g.setColor(Color.RED);
-		g.setFont(new Font("Cambria", Font.BOLD, 18));
-		g.drawString("BitCamp <3", 250, 150);
-		
-		g.setColor(Color.BLUE);
-		g.drawLine(100, 200, 500, 200);*/
+		while (!TextIO.eof()) {
+			
+			String str = TextIO.getln();
+			String[] string = str.split(" ");
+			String[] emptyString = new String[6];
+			
+			for(int i = 0; i < string.length; i++) {
+				emptyString[i] = string[i];
+			}
+			
+			for(int i= 0; i < emptyString.length; i++) {
+				switch(string[0]) {
+					case "L":
+						g.drawLine(Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3]), Integer.parseInt(string[4]));
+						break;
+					case "R":
+						g.drawRect(Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3]), Integer.parseInt(string[4]));
+						break;
+					case "FR":
+						g.fillRect(Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3]), Integer.parseInt(string[4]));
+						break;
+					case "S":
+						g.drawString(string[1], Integer.parseInt(string[2]),Integer.parseInt(string[3]));
+						break;
+					case "Chess":
+						drawChess(Integer.parseInt(string[1]), g);
+						break;
+					case "C":
+						g.setColor(new Color(Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3])));
+					case "Star":
+						star(g, Integer.parseInt(string[1]));
+						break;
+					case "O":
+						g.drawOval(Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3]), Integer.parseInt(string[4]));
+						break;
+					case "bh":
+						bhFlag(g);
+						break;
+					case "MM":
+						mickeyMouse(g);
+				}
+			}
+		}
 		
 		// *******************
 		w.setImage(img);
 	}
+	
+		public static void drawChess(int size, Graphics g) {
+			int x, y;
+
+			for (int row = 0; row < 8; row++) {
+
+				for (int col = 0; col < 8; col++) {
+					x = col * size;
+					y = row * size;
+					if ((row % 2) == (col % 2))
+						g.setColor(Color.WHITE);
+					else
+						g.setColor(Color.black);
+					g.fillRect(x, y, size, size);
+				}
+			}
+		}
+		
+		public static void star(Graphics g, int x) {
+			g.setColor(Color.BLACK);
+			
+			g.drawLine(4 * x, x, 2 * x, 6 * x);
+			g.drawLine(4 * x, x, 6 * x, 6 * x);
+			g.drawLine(x, 3 * x, 7 * x, 3 * x);
+			g.drawLine(x, 3 * x, 6 * x, 6 * x);
+			g.drawLine(2 * x, 6 * x, 7 * x, 3 * x);
+			
+		}
+		
+		public static void bhFlag(Graphics g) {
+			
+			g.setColor(Color.BLUE);
+			g.fillRect(370, 100, 200, 100);
+			g.setColor(Color.YELLOW);
+			g.fillRect(450, 125, 80, 50);
+			g.setColor(Color.BLUE);
+			g.drawLine(450, 125, 530, 175);
+			
+			
+		}
+		
+		public static void mickeyMouse(Graphics g) {
+			g.setColor(Color.BLACK);
+			g.fillOval(50, 50, 120, 120);//lijevo uho
+			g.fillOval(200, 50, 120, 120);//desno uho
+			g.setColor(Color.LIGHT_GRAY);
+			g.fillOval(65, 65, 100, 100);
+			g.fillOval(205, 65, 100, 100);
+			g.setColor(Color.BLACK);
+			g.fillOval(90, 120, 200, 200);//glava
+			g.setColor(Color.WHITE);
+			g.fillOval(130, 160, 40, 60);
+			g.fillOval(210, 160, 40, 60);
+			g.setColor(Color.DARK_GRAY);
+			g.fillOval(140, 180, 25, 30);
+			g.fillOval(215, 180, 25, 30);
+			g.setColor(Color.WHITE);
+			g.fillOval(150, 240, 80, 30);
+			g.setColor(Color.BLACK);
+			g.fillOval(200, 260, 20, 20);
+			g.fillOval(160, 260, 20, 20);
+		}
 }
